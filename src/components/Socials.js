b951d2c1db08fa2346c870e65cd90218da45a066
Mathias002 <moussetmathias@gmail.com>
2024-12-29 18:13:05 +0100

    frontend default template OK
    fonctionnal frontend with route transition cursor etc

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, {useContext} from 'react';
+// import icons
+import {
+  ImInstagram,
+  ImPinterest,
+} from 'react-icons/im'
+// cursor context
+import { CursorContext } from '../context/CursorContext';
 
 const Socials = () => {
-  return <div>Socials</div>;
+  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext)
+  return (
+  <div 
+    onMouseEnter={mouseEnterHandler}
+    onMouseLeave={mouseLeaveHandler}
+    className='hidden xl:flex ml-24'
+  >
+    <ul className='flex gap-x-4'>
+      <li>
+        <a href='http://www.instagram.com' target='_blank'>
+          <ImInstagram />
+        </a>
+      </li>
+      <li>
+        <a href='http://www.pinterest.com' target='_blank'>
+          <ImPinterest />
+        </a>
+      </li>
+    </ul>
+  </div>
+  );
 };
 
 export default Socials;
